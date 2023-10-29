10cc4b05100acec6e2d60083df106923a266ae20
Atul090 <999.atulsingh@gmail.com>
2023-10-29 15:19:39 +0530

    redux_toolkit_setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState,useEffect } from "react"
 import {fetchDataFromApi} from "./utils/api"
-
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const {url}=useSelector((state)=>state.home);
+
+
   useEffect(()=>{
     apiTesting();
   },[]);
@@ -11,12 +16,15 @@ function App() {
     fetchDataFromApi("/movie/popular")
       .then((res)=>{
         console.log(res);
+        dispatch(getApiConfiguration(res));
       });
   };
 
-  return (
-    <div className="App">App</div>
-  )
+  return <div className="App">
+    App
+    {url?.total_pages}
+  </div>
+  
 }
 
 export default App
